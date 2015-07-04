06fd92c0e736cfaccff3a08dc26035ada92d0bb1
alkaj <alkaj09@gmail.com>
2015-07-04 19:15:47 +0100

    visible 1

--- box/box.js
+++ box/box.js
@@ -1,10 +1,19 @@
 /* global angular */
 angular.module('box',['ngMaterial'])
-.directive('box',function(){
+.directive('kajBox',function(){
   'use strict';
   return {
     restrict:'E',
     templateUrl:'template.html',
-    replace:true
+    replace:true,
+    require:'ngModel',
+    scope:{
+      title:'@',
+      type:'@',
+      name:'@'
+    },
+    controller:function($scope,$element){
+      $scope.content="Le contenu de ma super aler te ira ici....";
+    }
   };
 });
